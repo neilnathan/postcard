b3b8fdb421df6d8a55a11411abf927640c423eef
Neil Nathan <neil.nathan@me.com>
2014-10-05 14:17:01 +0200

    add challenge 2

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterNessageTextField: UITextField!
     
@@ -33,8 +34,15 @@ class ViewController: UIViewController {
         messageLabel.text = enterNessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterNessageTextField.text = ""
         enterNessageTextField.resignFirstResponder()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal
         )
